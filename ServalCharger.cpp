628123912d809f497bf51ac855b0b68cf70eba7f
kuberlog <collinalexbell@gmail.com>
2021-03-16 08:36:29 -0500

    init commit

---
+++ ServalCharger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kuberlog on 3/16/21.
+//
+
+#include "ServalCharger.h"
---
+++ ServalCharger.h
@@ -0,0 +1,23 @@
+//
+// Created by kuberlog on 3/16/21.
+//
+
+#ifndef DAEMON___SERVALCHARGER_H
+#define DAEMON___SERVALCHARGER_H
+
+/*
+ * ServalCharger is what gives power to the System76 Serval Laptop
+ *
+ * Requirements: A power source that is in parallel with the Motor Power
+ * Options:
+ *      a) Seperate battery system from motors
+ *      b) Same battery system as motors
+ *      c) No battery system external to the Serval itself
+ */
+
+class ServalCharger {
+
+};
+
+
+#endif //DAEMON___SERVALCHARGER_H
